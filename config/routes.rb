0c933a364d289d044be55db7c1fb28790652ccb9
ponizi <ponalexis@gmail.com>
2019-08-20 21:47:08 +0200

    Third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :visitors
-  post '/index', :to => "visitors#create"
-  get '/index', :to => "static_pages#index"
+  resources :visitors 
+  root 'static_pages#index'
+  post '/', :to => "statics_pages#create"
+
 end
